aa8bb689d05730231b2127949ec15714de2ede36
Minliahmetova Violetta <vettaify@gmail.com>
2022-04-10 20:45:27 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8450;
+        int oneMile = 20;
+        int creditedMiles = ticketPrice / oneMile;
+        System.out.println(creditedMiles);
+    }
+}
